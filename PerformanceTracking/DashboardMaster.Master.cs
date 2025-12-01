7c66a1e35e57286be187de193510ac79f96cc700
Sakindi Manywa Ghislain <ghislainmanywa@gmail.com>
2025-12-01 22:20:58 +0200

    fixed the logout issue in the dashboard

--- PerformanceTracking/DashboardMaster.Master.cs
+++ PerformanceTracking/DashboardMaster.Master.cs
@@ -102,6 +102,25 @@ namespace PTMS
                     return "Login.aspx";
             }
         }
+
+        protected void lnkLogout_Click(object sender, EventArgs e)
+        {
+            // Clear all session variables
+            Session.Clear();
+            
+            // Abandon the session
+            Session.Abandon();
+            
+            // Clear authentication cookie if using forms authentication
+            if (Response.Cookies["ASP.NET_SessionId"] != null)
+            {
+                Response.Cookies["ASP.NET_SessionId"].Value = string.Empty;
+                Response.Cookies["ASP.NET_SessionId"].Expires = DateTime.Now.AddMonths(-20);
+            }
+            
+            // Redirect to login page
+            Response.Redirect("~/Login.aspx");
+        }
     }
 }
 
