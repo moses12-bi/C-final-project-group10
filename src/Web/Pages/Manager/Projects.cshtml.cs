a77c5cbe01c60b6d954379c4bc52cf49c4f10a81
moses12-bi <mosesebagabo01@gmail.com>
2025-12-10 11:03:34 +0200

    Add Manager Projects page

---
+++ src/Web/Pages/Manager/Projects.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Manager;
+
+public class ProjectsModel : PageModel
+{
+    public void OnGet()
+    {
+        // Projects page logic
+    }
+}
