8a549b6f61a15e24610e7fbd2803c0fd024f61a1
moses12-bi <mosesebagabo01@gmail.com>
2025-12-10 11:04:26 +0200

    Add Manager Settings page

---
+++ src/Web/Pages/Manager/Settings.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Manager;
+
+public class SettingsModel : PageModel
+{
+    public void OnGet()
+    {
+        // Settings page logic
+    }
+}
