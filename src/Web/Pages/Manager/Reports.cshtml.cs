026888a5bdb1da449b78c66e81a6bfd7ed66442f
moses12-bi <mosesebagabo01@gmail.com>
2025-12-10 11:04:02 +0200

    Add Manager Reports page

---
+++ src/Web/Pages/Manager/Reports.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Manager;
+
+public class ReportsModel : PageModel
+{
+    public void OnGet()
+    {
+        // Reports page logic
+    }
+}
