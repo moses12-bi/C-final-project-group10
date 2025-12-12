3bb4b444024a0b79dfa56501f029fc1b4b9a9599
moses12-bi <mosesebagabo01@gmail.com>
2025-12-12 12:08:58 +0200

    feat: Add project management domain models with GUID user references

---
+++ Models/TaskAssignment.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectM.Models
+{
+    public class TaskAssignment
+    {
+        public int Id { get; set; }
+        public int TaskId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UnassignedAt { get; set; }
+        public bool IsPrimaryAssignee { get; set; } = true;
+
+        // Navigation properties
+        public ProjectTask? Task { get; set; }
+        public User? User { get; set; }
+    }
+}
